98da4cc6a8bdb5edb1ac4f66e48b24482e7e1acc
Mariana <mariana.alves8384@gmail.com>
2022-08-20 21:38:51 -0300

    Arrumando perfil, scroll reveal, e projetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,5 +31,27 @@ function topFunction() {
 }
 
 
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.perfil', { delay: 700 })
+sr.reveal('.contatos', { delay: 700 })
+sr.reveal('.projetos', { delay: 700 })
+sr.reveal('.navbar', { delay: 1000 })
 
 
+function mouseEnter() {
+  document.getElementById("demo").innerHTML = "Tecnólogo em Análise e Desenvolvimento de Sistemas - Fatec João Mod, 2022- <br>Técnico Em Desenvolvimento de Sistemas - Etec Alfredo de Barros Santos, 2019-2021"
+}
+function mouseEnter2() {
+  document.getElementById("demo").innerHTML = "<ion-icon class='icone-perfil' name='logo-javascript'></ion-icon> <ion-icon class='icone-perfil' name='logo-html5'></ion-icon> <ion-icon class='icone-perfil' name='logo-css3'></ion-icon> <ion-icon class='icone-perfil' name='logo-python'></ion-icon> <ion-icon class='icone-perfil' name='server'></ion-icon>"
+}
+function mouseEnter3() {
+  document.getElementById("demo").innerHTML = "IT Essentials - Cisco, 2022"
+}
+function mouseEnter4() {
+  document.getElementById("demo").innerHTML = "Nenhuma experiência no mercado."
+}
+
+function mouseLeave() {
+  document.getElementById("demo").innerHTML = " "
+}
