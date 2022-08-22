8a25340749ca1c687fa329e2346dbe62775d6ed5
Mariana <mariana.alves8384@gmail.com>
2022-08-22 09:22:26 -0300

    arrumando a navbar e scroll top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,14 @@
-const lista = document.querySelectorAll('.lista');
-const home = document.querySelectorAll('#home');
+// mybutton = document.getElementById("myBtn");
 
-function ativarLink() {
-    lista.forEach((item)=>
-    item.classList.remove('ativo'))
-    this.classList.add('ativo')
-}
-lista.forEach((item) =>
-item.addEventListener('click', ativarLink))
+// window.onscroll = function() {scrollFunction()};
 
-
-
-
-mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
 
 function topFunction() {
@@ -55,3 +41,43 @@ function mouseEnter4() {
 function mouseLeave() {
   document.getElementById("demo").innerHTML = " "
 }
+
+
+let section = document.querySelectorAll('section');
+        let lists = document.querySelectorAll('.lista');
+        function activeLink(li) {
+            lists.forEach((item) => item.classList.remove('ativo'));
+            li.classList.add('ativo');
+        }
+        lists.forEach((item) =>
+            item.addEventListener('click', function(){
+                activeLink(this);
+            }));
+
+        window.onscroll = () => {
+            section.forEach(sec => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop - 50;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+
+                if (top >= offset && top < offset + height) {
+                    const target = document.querySelector(`[href='#${id}']`).parentElement;
+                    activeLink(target);
+                }
+            })
+        };
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
